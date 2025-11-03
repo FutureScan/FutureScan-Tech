97483237c1ceaa5a9b0f21df27eea96b3f686344
Vibecode Terminal <terminal@vibecodeapp.com>
2025-11-03 00:22:36 +0000

    Sandbox autosave

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Navigation } from "@/components/Navigation";
+import { ClientLayout } from "@/components/ClientLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,10 +29,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black text-white`}
       >
-        <Navigation />
-        <main className="pb-20 lg:pb-0 lg:ml-64">
-          {children}
-        </main>
+        <ClientLayout>
+          <Navigation />
+          <main className="pb-20 lg:pb-0 lg:ml-64">
+            {children}
+          </main>
+        </ClientLayout>
       </body>
     </html>
   );
